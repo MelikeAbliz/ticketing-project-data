d26d5a0c6f759c49c132daa7971cb3c2905c6115
MelikeAbliz <medine20171121@gmail.com>
2024-05-16 13:30:32 +0300

    ticketing project part1

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,24 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication //this includes @Configuration
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
     }
 
+
+    //I am trying to add bean in the container through @Bean annotation
+    //create a class annotated with @Configuration
+    //write a method return the object that you are trying to add in the container
+    //annotate this method with @Bean
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,16 +1,47 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
     @Override
     public List<RoleDTO> listAllRoles() {
-        return null;
+        //controller call me, request all RoleDto, so it can show in the drop_down in the ui
+        //I need to make a call to db and get all the roles from table
+        //go to repository and find a service(method) which gives me the roles from db
+        //how I will call any service here? dependency injection
+
+        List<Role> roleList = roleRepository.findAll();//convert Role to RoleDto
+
+        //I have Role entities from DB
+        //I need to convert those Role entities to DTOs
+        //I need to use modelMapper
+        //I already created a class called RoleMapper and there are method for me that make this conversion,DI
+
+        List<RoleDTO> roleDTOList = roleList.stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+
+        return roleDTOList;
+
+        //when I run and get : (Relying upon circular references is discouraged and they are prohibited by default
+        // . Update your application to remove the dependency cycle between beans.) exception
+        //go to RoleDtoConverter class add @lazy to roleService
     }
 
     @Override
