79909407e35e05762e0911722344cb61be397837
MelikeAbliz <medine20171121@gmail.com>
2024-05-17 09:38:38 +0300

    completed user part and @PrePersist,@Column(nullable=false,updatable = false)

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,11 +1,9 @@
 package com.cydeo.entity;
 
 import lombok.*;
+import org.springframework.lang.Nullable;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -20,9 +18,26 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Boolean isDeleted = false;
+    @Column(nullable=false,updatable = false)//ignore fields,just keep the data for creation time,do not change when update
     private LocalDateTime insertDateTime;
+    @Column(nullable=false,updatable = false)
     private Long insertUserId;
+    @Column(nullable=false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable=false)
     private Long lastUpdateUserId;
+
+    @PrePersist
+    private void prePersist() {
+        this.insertDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
+    @PreUpdate
+    private void preUpdate() {
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 
 }
